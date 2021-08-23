aaa119d37da7fd5d85fe28ac7e8df38f8c4ad06b
Heiko Voigt <hvoigt@hvoigt.net>
2021-08-23 16:44:36 +0200

    initial formulae for tucnak
    http://tucnak.nagano.cz/wiki/Main_Page

---
+++ Formula/tucnak.rb
@@ -0,0 +1,37 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Tucnak < Formula
+  desc "Tucnak - hamradio contest log"
+  homepage "http://tucnak.nagano.cz/wiki/Main_Page"
+  url "http://tucnak.nagano.cz/tucnak-4.11.tar.gz"
+  sha256 "0aa1661a2a65d43218e9e17a373078efa417cabeeacfe25fa15f9c2840567946"
+
+  depends_on "libzia"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test tucnak`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
